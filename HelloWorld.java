16293249270db0b1e474b6c36c7049223734ea87
TobWo <188315674+TobWo@users.noreply.github.com>
2024-11-13 11:46:48 +0100

    Update HelloWorld.java
    neue Zeile

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");  // Diese Zeile wird ausgeführt
+        System.out.println("Welcome to GitHub Actions!");  // Deine neue Zeile
     }
 }
